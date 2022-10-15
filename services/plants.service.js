56f41aaaf6c0bafcf6e73f2054e34d01df276dfe
Nicolas Peñaloza <57502001+nicopenaloza@users.noreply.github.com>
2022-10-15 14:19:55 -0300

    Home page

---
+++ services/plants.service.js
@@ -0,0 +1,23 @@
+export class Plants {
+
+    topPick = [];
+
+    getTopPicks() {
+        return new Promise(resolve => 
+            setTimeout(() => {
+                this.topPick = [{ "id": 1, "name": "Rocket", "picture": "https://www.rhs.org.uk/getmedia/ada7442e-1241-4dfb-a912-67fe5861b9cd/Salad-rocket940x627.jpg?width=940&height=627&ext=.jpg" }, { "id": 2, "name": "Rocket", "picture": "https://www.rhs.org.uk/getmedia/ada7442e-1241-4dfb-a912-67fe5861b9cd/Salad-rocket940x627.jpg?width=940&height=627&ext=.jpg" }, { "id": 3, "name": "Rocket", "picture": "https://www.rhs.org.uk/getmedia/ada7442e-1241-4dfb-a912-67fe5861b9cd/Salad-rocket940x627.jpg?width=940&height=627&ext=.jpg" }]
+                resolve(this.topPick);
+            }, 1000)
+        )
+    }
+
+    getTopCategories() {
+        return new Promise(resolve =>
+            setTimeout(() => {
+                this.topPick = [{ "id": 1, "name": "Rocket", "picture": "https://www.rhs.org.uk/getmedia/ada7442e-1241-4dfb-a912-67fe5861b9cd/Salad-rocket940x627.jpg?width=940&height=627&ext=.jpg" }, { "id": 2, "name": "Rocket", "picture": "https://www.rhs.org.uk/getmedia/ada7442e-1241-4dfb-a912-67fe5861b9cd/Salad-rocket940x627.jpg?width=940&height=627&ext=.jpg" }, { "id": 3, "name": "Rocket", "picture": "https://www.rhs.org.uk/getmedia/ada7442e-1241-4dfb-a912-67fe5861b9cd/Salad-rocket940x627.jpg?width=940&height=627&ext=.jpg" }]
+                resolve(this.topPick);
+            }, 1000)
+        )
+    }
+
+}
