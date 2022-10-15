283791c5a0173d8ebfb5377461b244dc90141cc1
Platinum <platinumemirate@gmail.com>
2022-10-15 19:16:00 +0100

    PlantCard

--- components/plant-card.jsx
+++ components/plant-card.jsx
@@ -1,7 +1,12 @@
 
 export default function PlantCard({ plant }) {
-  return(
-    <div className="card">
+  return (
+    <div className="card" key={plant.id} onClick={() => location.href = `plant/${plant.id}`} >
+      <IMg src={plant.picture} class="card-img-top" alt={plant.name} />
+        <div class="card-body">
+          <h5 class="card-title">{plant.name}</h5>
+          <p class="card-text">{plant.description}</p>
+        </div>
     </div>
   )
 }
