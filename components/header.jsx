88c27f42a98bdecc37e5f4e68a216a96524dc370
Platinum <platinumemirate@gmail.com>
2022-10-15 23:07:46 +0100

    heaeder

--- components/header.jsx
+++ components/header.jsx
@@ -1,29 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Header() {
   return (
     <div className='bg-green '>
       <nav class="navbar navbar-expand-lg container text-white">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Grow Green</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
+          <a class="navbar-brand" href="/">
+            <h2>Grow Green</h2>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDrop" aria-controls="navbarDrop" aria-expanded="false" aria-label="Toggle">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse">
-            <ul class="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">Shop</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">Plants</a>
-              </li>
-            </ul>
-            <div class="d-flex">
-              <a class="btn text-white" > <i class="bi bi-basket"></i> </a>
+          <div class="collapse navbar-collapse" id='navbarDrop'>
+            <div class="ms-auto me-auto d-flex gap-5">
+              <a href="/plant">Discover Plants</a>
+              <a href="/shop">Shop Resources</a>
             </div>
+            <a class="btn text-white" href='/cart'> <i class="bi bi-basket"></i> </a>
           </div>
         </div>
       </nav>
