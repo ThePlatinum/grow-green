a9bc8c1163d94052228e3c2d156041da9f1c5eaa
Platinum <platinumemirate@gmail.com>
2022-10-15 13:18:12 +0100

    basic setup

---
+++ pages/shop.js
@@ -0,0 +1,9 @@
+import {container} from '../styles/Home.module.css'
+
+export default function Shop() {
+  return (
+    <div className={container}>
+      The shop Page
+    </div>
+  )
+}
---
+++ utils/consts.js
@@ -0,0 +1,6 @@
+const consts = {
+  BACKEND_URL: `http://localhost:8000/api/`, // `https://ise.mantenar.com/api/`
+  IMAGE__URL: `http://localhost:8000`, // `https://ise.mantenar.com`
+}
+
+export default consts;
