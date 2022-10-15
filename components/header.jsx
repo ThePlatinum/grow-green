44a80b7477f8537be0e6d740cc2c5268930f71ab
Nicolas Peñaloza <57502001+nicopenaloza@users.noreply.github.com>
2022-10-15 19:38:42 -0300

    header animation

--- components/header.jsx
+++ components/header.jsx
@@ -6,14 +6,14 @@ export default function Header() {
       <nav class="navbar navbar-expand-lg container text-white">
         <div class="container-fluid">
           <a class="navbar-brand" href="/">
-            <h2>Grow Green</h2>
+            <h2 className='text-light'>Grow Green</h2>
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDrop" aria-controls="navbarDrop" aria-expanded="false" aria-label="Toggle">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="d-flex gap-5 align-items-center">
-            <a href="/plant">Discover Plants</a>
-            <a href="/shop">Shop Resources</a>
+            <a className='navbar-item text-light' href="/plant">Discover Plants</a>
+            <a className='navbar-item text-light' href="/shop">Shop Resources</a>
             <a class="btn btn-outline-light position-relative" href='/shop/cart'>
               <i class="bi bi-basket"></i>
               <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
